e22fb705691231793ac07c8e595ce484e3031dc0
杜冠魁 <hello@dignite.com>
2023-10-15 17:26:02 +0900

    add a RulesEngine Example

---
+++ RulesEngineSample/RulesEngineSample/Buyer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulesEngineSample
+{
+    public class Buyer
+    {
+        public Buyer()
+        {
+        }
+
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public bool Authenticated { get; set; }
+    }
+
+    public class VIP
+    {
+        public VIP()
+        {
+        }
+
+        public int Id { get; set; }
+        public bool IsVIP { get; set; }
+    }
+    public class Discount
+    {
+        public Discount()
+        {
+        }
+
+        public double Value
+        {
+            get; set;
+        }
+    }
+}
---
+++ RulesEngineSample/RulesEngineSample/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using Newtonsoft.Json;
+using RulesEngine.Models;
+using RulesEngineSample;
+
+var rulesStr = @"[{
+	""WorkflowName"": ""Test"",	
+	""GlobalParams"": [{
+		""Name"": ""discountValue"",
+		""Expression"": ""discountA.Value""
+	}],
+	""Rules"": [{
+			""RuleName"": ""CheckAge"",
+			""Expression"": ""buyerA.Age < 18"",
+			""Actions"": {
+				""OnSuccess"": {
+					""Name"": ""OutputExpression"",
+					""Context"": {
+						""Expression"": ""discountValue * 0.9""
+					}
+				}
+			}
+		},
+		{
+			""RuleName"": ""CheckVIP"",
+			""Expression"": ""vipA.IsVIP == true"",
+			""Actions"": {
+				""OnSuccess"": {
+					""Name"": ""OutputExpression"",
+					""Context"": {
+						""Expression"": ""discountValue * 0.9""
+					}
+				}
+			}
+		}
+	]
+}]";
+
+var rp1 = new RuleParameter("buyerA", new Buyer
+{
+    Id = 666,
+    Age = 16
+});
+
+var rp2 = new RuleParameter("vipA", new VIP
+{
+    Id = 666,
+    IsVIP = true
+});
+
+var rp3 = new RuleParameter("discountA", new Discount
+{
+    Value = 1.0
+});
+
+
+var workflows = JsonConvert.DeserializeObject<List<Workflow>>(rulesStr)!;
+var bre = new RulesEngine.RulesEngine(workflows.ToArray());
+
+List<RuleResultTree> resultList = await bre.ExecuteAllRulesAsync("Test", rp1,rp2,rp3);
+var discount = 1.0;
+foreach (var item in resultList)
+{
+    if (item.ActionResult != null && item.ActionResult.Output != null)
+    {
+        Console.WriteLine($"{item.Rule.RuleName} Discount Offer：{item.ActionResult.Output}");
+        discount = discount * (double)item.ActionResult.Output;
+    }
+}
+Console.WriteLine($"Final Discount Offer:{discount}");
+
